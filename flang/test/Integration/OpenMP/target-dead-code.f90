aa8e49327ea77afcfca2a8e1da4776c914ef22ad
Sergio Afonso <Sergio.AfonsoFumero@amd.com>
2026-08-10 13:31:26 +0100

    Address review comments

--- flang/test/Integration/OpenMP/target-dead-code.f90
+++ flang/test/Integration/OpenMP/target-dead-code.f90
@@ -1,3 +1,11 @@
+!===----------------------------------------------------------------------===!
+! This directory can be used to add Integration tests involving multiple
+! stages of the compiler (for eg. from Fortran to LLVM IR). It should not
+! contain executable tests. We should only add tests here sparingly and only
+! if there is no other way to test. Repeat this message in each test that is
+! added to this directory and sub-directories.
+!===----------------------------------------------------------------------===!
+
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp %s -o - | tco -test-gen | FileCheck %s --check-prefixes=HOST,ALL
 ! RUN: %flang_fc1 -emit-hlfir -fopenmp -fopenmp-is-target-device %s -o - | tco -test-gen | FileCheck %s --check-prefixes=DEVICE,ALL
 
